68f9ca444833c4decd873679bfb064ced705efb7
kauanderlon <253342918+wesleygois-hue@users.noreply.github.com>
2026-08-28 22:33:33 -0300

    Feat: Modificada a função de criação de conta para contemplar também a adição automática de uma agência

--- conta.py
+++ conta.py
@@ -1,8 +1,8 @@
 #RESPONSABILIDADES: N CONTA, AGENCIA, DADOS SOBRE SALDO, EXTRATO, DEPÓSITO, SAQUE.
 
-def n_conta(nc) : #Atribui o número da conta em sua criação.
-    nc = 1;
-    return nc
-
-
+def n_conta_agencia(nc) : #Atribui o número da conta em sua criação junto à uma
+                  #agência, que, por questão de simplicidade, será sempre a mesma.
+    nc = nc + 1;
+    na = 2;
+    return (nc, na)
 

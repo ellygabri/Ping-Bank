5c62512668c7fbd06ed45f5390855596dc8647d2
ellygabri <164203372+ellygabri@users.noreply.github.com>
2026-09-03 19:45:12 -0300

    Update cliente.py
    Criada a função para cadastro do Cliente

--- cliente.py
+++ cliente.py
@@ -1,1 +1,11 @@
+"""
+Etapa 1:
+Criação da função para cadastro do cliente
+"""
 
+def cadastrar_cliente (nome, cpf, num_contato, endereco, data_nascimento, email):
+    if nome.strip() == "" or cpf.strip() == "" or num_contato.strip() == "" or endereco.strip() == "" or data_nascimento.strip() == "" or email.strip() == "":
+        return False
+    else:
+        dados_cliente = (nome, cpf, num_contato, endereco, data_nascimento, email)
+        return dados_cliente

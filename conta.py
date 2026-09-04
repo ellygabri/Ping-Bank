77fb045b09c230376da12d8e8c953fb1ad710fe2
ellygabri <164203372+ellygabri@users.noreply.github.com>
2026-09-03 21:17:40 -0300

    Modificadas as funções  e Criada a função "consultar_saldo"
    A função saque estava com erro no operador de comparação e o arquivo conta.py não contava com a função para a consulta do saldo.

--- conta.py
+++ conta.py
@@ -1,24 +1,29 @@
 #RESPONSABILIDADES: N CONTA, AGENCIA, DADOS SOBRE SALDO, EXTRATO, DEPÓSITO, SAQUE.
 
-def cria_conta_agencia() :      #Atribui o número da conta em sua criação junto à uma
+def criar_conta_agencia() :      #Atribui o número da conta em sua criação junto à uma
                                     #agência, que, por questão de simplicidade, será sempre a mesma.
-    num_conta = 1
+    num_conta = 123456
     num_agencia = 2
     return (num_conta, num_agencia, 0) #Obs.: 0 -> Saldo inicial da conta.
 
-def deposito(saldo, deposito) : #Aumenta o saldo da conta com o valor do depósito.
-    if deposito > 0:
-        saldo = saldo + deposito
+def deposito(saldo, valor_deposito) : #Aumenta o saldo da conta com o valor do depósito.
+    if valor_deposito > 0:
+        saldo = saldo + valor_deposito
         return saldo
     else:
       return False
 
-def saque(saldo, saque) : #Diminui o saldo da conta com o valor do saque.
-     if saldo <= saque:
-      saldo = saldo - saque
+def saque(saldo, valor_saque) : #Diminui o saldo da conta com o valor do saque.
+     if saldo >= valor_saque:
+      saldo = saldo - valor_saque
       return saldo
      else:
        return False
+
+#Adicionado a função: Consultar Saldo
+
+def consultar_saldo(saldo):
+   print("Saldo atual: R$", saldo)
 
 
 

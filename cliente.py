7ce94da86166fcaea1a358390e84e2611fd6448e
ellygabri <164203372+ellygabri@users.noreply.github.com>
2026-09-16 18:03:49 -0300

    Atualizações de cliente.py para a etapa 2
    Criação das funções buscar_cliente, listar_clientes.
    Atualização da função cadastrar_cliente.

--- cliente.py
+++ cliente.py
@@ -1,11 +1,34 @@
-"""
-Etapa 1:
-Criação da função para cadastro do cliente
-"""
 
-def cadastrar_cliente (nome, cpf, num_contato, endereco, data_nascimento, email):
-    if nome.strip() == "" or cpf.strip() == "" or num_contato.strip() == "" or endereco.strip() == "" or data_nascimento.strip() == "" or email.strip() == "":
-        return False
-    else:
-        dados_cliente = (nome, cpf, num_contato, endereco, data_nascimento, email)
-        return dados_cliente
+def buscar_cliente(clientes, cpf):
+    for cliente in clientes:
+        if cliente[1] == cpf:
+            return cliente
+        return None
+
+def cadastrar_cliente(clientes):
+    nome = input("Informe o nome: ")
+    cpf = input("Informe o CPF: ")
+
+    if buscar_cliente(clientes, cpf) != None:
+        print("Cliente já cadastrado")
+        return
+    contato = input("Informe seu número para contato: ")
+    endereco = input("Informe seu endereço: ")
+    data_nascimento = input("Informe sua data de nascimento: ")
+    email = input("Informe um e-mail válido: ")
+
+    cliente = [nome, cpf, contato, endereco, data_nascimento, email]
+    clientes.append(cliente)
+
+    print("Cliente cadastrado com sucesso!")
+
+def listar_clientes(clientes):
+    if len(clientes) == 0:
+        print("Não há clientes cadastrados!")
+        return
+    print("==== CLIENTES CADASTRADOS ====")
+
+    for cliente in clientes:
+        print("Nome: ", cliente[0])
+        print("CPF: ", cliente[1])
+        print("==============================")

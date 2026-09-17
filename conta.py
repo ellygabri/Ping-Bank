808ff7446e2e67c6c665638a78c232d0971c1438
kauanderlon <253342918+wesleygois-hue@users.noreply.github.com>
2026-09-17 11:56:01 -0300

    Feat: Atualizações do núcleo de funcionamento da função criar_conta()
    - Utilização de listas para a adição de múltiplas contas
    - Adição do tipo de conta (individual ou conjunta)
    - Validações acerca da existência de dados e erros de digitação
    - Adição da opção de haver um valor inicial
    - Contabilização da conta de acordo com a quantidade de contas existentes, bem como a validação da quantidade previamente existente na agência (a fim de evitar duplicidade)

--- conta.py
+++ conta.py
@@ -1,10 +1,45 @@
-#RESPONSABILIDADES: N CONTA, AGENCIA, DADOS SOBRE SALDO, EXTRATO, DEPÓSITO, SAQUE.
+from cliente import clientes
+contas = []
 
-def criar_conta_agencia() :      #Atribui o número da conta em sua criação junto à uma
-                                    #agência, que, por questão de simplicidade, será sempre a mesma.
-    num_conta = 123456
-    num_agencia = 2
-    return (num_conta, num_agencia, 0) #Obs.: 0 -> Saldo inicial da conta.
+def criar_conta() :   
+
+    titulares = [] #Lista de apoio para o cadastro dos titulares da conta.
+    num_agencia = input("Informe o número da agência: ")
+    tipo_conta = int(input("Informe o tipo de conta (1 - Individual, 2 - Conjunta): "))
+    if tipo_conta == 2:
+        qtd_titulares = int(input("Informe a quantidade de titulares da conta: "))
+    else:
+        qtd_titulares = 1
+    tentativas = 0 
+
+    while tentativas < qtd_titulares: #Loop de cadastro dos titulares, tolerante à erros de digitação.
+        cpf = input("Informe o CPF do titular da conta: ")
+        for cliente in clientes: #Checagem da existência do cliente no banco de dados.
+            if cliente[1] == cpf:
+                titulares.append(cpf)
+                tentativas += 1
+            else:
+                print("Cliente não encontrado. Por favor, tente novamente.")
+
+    for agencia in agencias: #Checagem da existência da agência no banco de dados.
+        if agencia[0] == num_agencia:
+            resultado = agencia
+        else:
+            resultado = 1
+    num_conta = resultado[0][1] + 1 #Adição do número da conta considerando a sequência de números de conta já existentes na agência digitada.
+            
+    opcao_valor_inicial = int(input("Deseja informar um valor inicial para a conta? (1 - Sim, 2 - Não): "))
+    if opcao_valor_inicial == 1:
+        saldo = float(input("Informe o valor inicial da conta: "))
+    else:
+        saldo = 0
+
+    conta = [num_conta, num_agencia, tipo_conta, saldo, titulares]
+    contas.append(conta)
+    
+    return (conta) 
+
+
 
 def deposito(saldo, valor_deposito) : #Aumenta o saldo da conta com o valor do depósito.
     if valor_deposito > 0:

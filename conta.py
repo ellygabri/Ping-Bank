463e41dd3157f2b9e5ce5207170603dcbdce4a6c
kauanderlon <253342918+wesleygois-hue@users.noreply.github.com>
2026-09-19 11:01:40 -0300

    Feat: Alterações extensivas nas funções de saque, depósito e consulta de saldo, bem como alterações menores em criar conta e adição de funções de suporte
    Sobre as funções...
    
    – Depósito:
    > Adicionada a verificação de conta antes do depósito
    > Adicionada a validação de CPF (medida de segurança, embora rudimentar)
    > Alinhada a consulta de acordo com o índice, a fim de garantir a exatidão.
    
    – Saque:
    > Adicionada a verificação de conta antes do depósito
    > Adicionada a validação de CPF (medida de segurança, embora rudimentar)
    > Ajuste menor na verificação de validade para o depósito
    > Alinhada a consulta de acordo com o índice, a fim de garantir a exatidão.
    
    – Consultar saldo
    > Adicionada a verificação de conta antes da consulta, bem como a verificação de segurança.
    > Alinhada a consulta de acordo com o índice, a fim de garantir a exatidão.
    
    – Criar conta
    > Removida a compreensão de listas utilizada pré-confirmação de invalidez na etapa
    > Lista de apoio adicionada a fim de substituir a funcionalidade interna anterior
    
    – Verificador de existência
    > Criado a partir da necessidade de reutilização em outras partes do código.
    > Utiliza-se do princípio chave de alinhamento de dados atual: os índices alinhados
    > Na varredura, garante que os valores procurados não só correspondem aos inseridos, mas também, estão alinhados quanto aos índices.
    
    – Verificação de segurança
    > A fim de manter um controle de acesso, ainda que rudimentar, foi criada uma verificação que utiliza do CPF para permitir (ou não) o acesso.
    – Carece de reforma urgente, visto que a maneira de atribuir os titulares em criar_conta() é falha e não garante a integridade nem dos cadastrados, nem da verificação.

--- conta.py
+++ conta.py
@@ -6,7 +6,22 @@ saldos = []
 titulares = []
 
 
-def criar_conta() :   
+def verificador_existencia(num_conta, num_agencia): #Verifica a existência da conta e da agência no banco de dados.
+
+    verificar_existencia = 0
+    for indice_igual in range(len(num_contas)): #Navega por toda a lista a fim de constatar a existência dos dados.
+        if num_contas[indice_igual] == num_conta and num_agencias[indice_igual] == num_agencia: #É de extrema importância alinhar os índices para garantir exatidão na checagem.
+            return 1, indice_igual 
+    return 0, None
+
+def verificacao_seguranca(cpf_validacao): #Checagem de segurança (espécie de senha). Sujeito à alterações, pois a titulação feita na criação de conta possui falhas graves.
+
+    for titular in titulares: 
+        if titular == cpf_validacao:
+            return 1
+    return 0
+
+def criar_conta(): #Criação da conta e adição dos dados relacionados.   
 
     tipo_conta = int(input("Informe o tipo de conta (1 - Individual, 2 - Conjunta): "))
     if tipo_conta == 2:
@@ -25,69 +40,98 @@ def criar_conta() :
                 print("Cliente não encontrado. Por favor, tente novamente.")
 
     num_agencia = input("Informe o número da agência: ")
-    regulador_agencia_conta = [agencia for agencia in agencias if num_agencias == num_agencia] #Necessário para garantir a sequência numeral das contas.
+    regulador_agencia_conta = []
+    for agencia in num_agencias: #Necessário para garantir a sequência numeral correta das contas.
+        if agencia == num_agencia:  
+            regulador_agencia_conta.append(agencia)
+     
     if len(regulador_agencia_conta) > 0: #Checagem da existência da agência no banco de dados.
         num_conta = len(regulador_agencia_conta) + 1 #Adição do número da conta considerando a sequência de números de conta já existentes na agência digitada.
     else:
-        num_conta = 1
+        num_conta = 1 #Adição da primeira conta da nova agência. 
 
             
-    opcao_valor_inicial = int(input("Deseja informar um valor inicial para a conta? (1 - Sim, 2 - Não): "))
+    opcao_valor_inicial = int(input("Deseja informar um valor inicial para a conta? (1 - Sim, 2 - Não): ")) #Possibilidade de começar com um saldo inicial.
     if opcao_valor_inicial == 1:
         saldo = 0
         while saldo <= 0:
             saldo = float(input("Informe o valor inicial da conta: "))
     else:
         saldo = 0
+
+    #Finalização. Todas as informações são adicionadas na última posição das listas. Necessário para o alinhamento dos índices.
     num_contas.append(num_conta)
+    num_agencias.append(num_agencia)
     tipo_contas.append(tipo_conta)
     saldos.append(saldo)
-    num_agencias.append(num_agencia) 
+ 
 
     return 
 
 
 
-# def deposito() : #Aumenta o saldo da conta com o valor do depósito.
-#     valor_deposito = float(input("Insira o valor a ser depositado: "))
-#     num_conta = input("Digite o número da conta: ")
-#     conta_da_movimentacao = []
-#     for conta in contas: #Checagem da existência da conta no banco de dados.
-#         if conta[1] == num_conta:
-#             conta_da_movimentacao = conta
-#         else:
-#             print("Conta não encontrada. Por favor, tente novamente.")
-#         return
+def deposito(): #Aumenta o saldo da conta com o valor do depósito.
 
-#     num_agencia = input("Digite o número da agência: ")
-#     if conta_da_movimentacao[1] != num_agencia: #Checagem da correspondência entre a agência e a conta digitadas.
-#         print("Agência não correspondente à conta informada. Por favor, tente novamente.")
-#         return
+    num_conta = input("Digite o número da conta: ")
+    num_agencia = input("Digite o número da agência: ")
+    verificar_existencia, indice = verificador_existencia(num_conta, num_agencia) #Verifica a existência da conta e da agência no banco de dados.
+    if verificar_existencia != 1:
+        print("Os dados não correspondem. Por favor, tente novamente.")
+        return
+    
+    cpf_validacao = input("Por favor, digite o CPF do titular da conta: ") #Checagem de segurança (espécie de senha).
+    if (verificacao_seguranca(cpf_validacao)) != 1:
+        print("Transação não autorizada.")
+        return
 
-#     cpf = input("Para fins de verificação, por favor, informe o CPF do titular: ")
-#     for titulares in conta_da_movimentacao: #Checagem da existência do cliente no banco de dados.
-#         if conta_da_movimentacao[4][0] != cpf:
-#             print("Movimento não autorizado.")
-#             return
-#         else:            
-#             if valor_deposito > 0:
-                
-#             else:
-#                 print("Valor inválido. Por favor, tente novamente.")    
-#                 return
-#         return 
+    valor = float(input("Digite o valor a ser depositado: "))
+    while valor <= 0: #Evitando valores inválidos.
+        print("Valor inválido. Por favor, tente novamente.")
+        valor = float(input("Digite o valor a ser depositado: "))
 
-def saque(saldo, valor_saque) : #Diminui o saldo da conta com o valor do saque.
-     if saldo >= valor_saque:
-      saldo = saldo - valor_saque
-      return saldo
-     else:
-       return False
+    saldos[indice] += valor #Finalização.
+    print("Depósito realizado com sucesso.")
+    return 
 
-#Adicionado a função: Consultar Saldo
+def saque(): #Diminui o saldo da conta com o valor do saque.
 
-def consultar_saldo(saldo):
-   print("Saldo atual: R$", saldo)
+    num_conta = input("Digite o número da conta: ")
+    num_agencia = input("Digite o número da agência: ")
+    verificar_existencia, indice = verificador_existencia(num_conta, num_agencia) #Verifica a existência da conta e da agência no banco de dados.
+    if verificar_existencia != 1:
+        print("Os dados não correspondem. Por favor, tente novamente.")
+        return
+    
+    cpf_validacao = input("Por favor, digite o CPF do titular da conta: ") #Checagem de segurança (espécie de senha).
+    if (verificacao_seguranca(cpf_validacao)) != 1:
+        print("Transação não autorizada.")
+        return
+
+    valor_saque = float(input("Digite o valor a ser sacado: "))
+    while valor_saque < 0 or valor_saque > saldos[indice]: #Evitando valores inválidos.
+        print("Valor inválido. Por favor, tente novamente.")
+        valor_saque = float(input("Digite o valor a ser sacado: "))
+
+    saldos[indice] -= valor_saque #Finalização.
+    print("Saque realizado com sucesso.")
+    return
+
+def consultar_saldo(): #Consulta o saldo da conta.
+
+    num_conta = input("Digite o número da conta: ")
+    num_agencia = input("Digite o número da agência: ")
+    verificar_existencia, indice = verificador_existencia(num_conta, num_agencia) #Verifica a existência da conta e da agência no banco de dados.
+    if verificar_existencia != 1:
+        print("Os dados não correspondem. Por favor, tente novamente.")
+        return
+    
+    cpf_validacao = input("Por favor, digite o CPF do titular da conta: ") #Checagem de segurança (espécie de senha).
+    if (verificacao_seguranca(cpf_validacao)) != 1:
+        print("Transação não autorizada.")
+        return
+    
+    print("Saldo atual: R$", saldos[indice]) #Finalização.
+    return 
 
 
 

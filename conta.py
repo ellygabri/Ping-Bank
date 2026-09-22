6d8471ee55bcc31ce06e76173c1ae4a1d6b71db2
kauanderlon <253342918+wesleygois-hue@users.noreply.github.com>
2026-09-22 18:31:53 -0300

    Feat: substituição do uso de índices de listas para manipulação de dados por tuplas
    Os dados passam a ser unificados novamente, dessa vez, em tuplas. As alterações pertinentes são feitas por meio da substituição e/ou adição de elementos na lista de interesse.

--- conta.py
+++ conta.py
@@ -1,23 +1,20 @@
 from cliente import cpf_clientes
-num_contas = []
-num_agencias = []
-tipo_contas = []
-saldos = []
-titulares = []
+
+
+contas = []
 
 
 def verificador_existencia(num_conta, num_agencia): #Verifica a existência da conta e da agência no banco de dados.
 
-    verificar_existencia = 0
-    for indice_igual in range(len(num_contas)): #Navega por toda a lista a fim de constatar a existência dos dados.
-        if num_contas[indice_igual] == num_conta and num_agencias[indice_igual] == num_agencia: #É de extrema importância alinhar os índices para garantir exatidão na checagem.
-            return 1, indice_igual 
+    for conta in range(len(contas)): #Navega por toda a lista a fim de constatar a existência dos dados.
+        if contas[conta][0] == num_conta and contas[conta][1] == num_agencia: #É de extrema importância alinhar os índices para garantir exatidão na checagem.
+            return 1, conta 
     return 0, None
 
-def verificacao_seguranca(cpf_validacao): #Checagem de segurança (espécie de senha). Sujeito à alterações, pois a titulação feita na criação de conta possui falhas graves.
+def verificacao_seguranca(cpf_validacao): #Checagem de segurança (uma espécie de senha).
 
-    for titular in titulares: 
-        if titular == cpf_validacao:
+    for titular in contas: 
+        if cpf_validacao in titular[4]: 
             return 1
     return 0
 
@@ -29,20 +26,21 @@ def criar_conta(): #Criação da conta e adição dos dados relacionados.
     else:
         qtd_titulares = 1
     tentativas = 0 
+    titulares = ()
 
     while tentativas < qtd_titulares: #Loop de cadastro dos titulares, tolerante à erros de digitação.
         cpf = input("Informe o CPF do titular da conta: ")
-        for cpf_cliente in cpf_clientes: #Checagem da existência do cliente no banco de dados.
-            if cpf_cliente == cpf:
-                titulares.append(cpf)
-                tentativas += 1
-            else:
-                print("Cliente não encontrado. Por favor, tente novamente.")
+        #Checagem da existência do cliente no banco de dados.
+        if cpf in cpf_clientes:
+            titulares = titulares + (cpf,)
+            tentativas += 1
+        else:
+            print("Cliente não encontrado. Por favor, tente novamente.")
 
     num_agencia = input("Informe o número da agência: ")
     regulador_agencia_conta = []
-    for agencia in num_agencias: #Necessário para garantir a sequência numeral correta das contas.
-        if agencia == num_agencia:  
+    for agencia in contas: #Necessário para garantir a sequência numeral correta das contas.
+        if agencia[1] == num_agencia:  
             regulador_agencia_conta.append(agencia)
      
     if len(regulador_agencia_conta) > 0: #Checagem da existência da agência no banco de dados.
@@ -54,18 +52,13 @@ def criar_conta(): #Criação da conta e adição dos dados relacionados.
     opcao_valor_inicial = int(input("Deseja informar um valor inicial para a conta? (1 - Sim, 2 - Não): ")) #Possibilidade de começar com um saldo inicial.
     if opcao_valor_inicial == 1:
         saldo = 0
-        while saldo <= 0:
+        while saldo < 0:
             saldo = float(input("Informe o valor inicial da conta: "))
     else:
         saldo = 0
 
-    #Finalização. Todas as informações são adicionadas na última posição das listas. Necessário para o alinhamento dos índices.
-    num_contas.append(num_conta)
-    num_agencias.append(num_agencia)
-    tipo_contas.append(tipo_conta)
-    saldos.append(saldo)
- 
-
+    #Finalização. Todas as informações são adicionadas na última posição das listas.
+    contas.append((num_conta, num_agencia, tipo_conta, saldo, titulares))
     return 
 
 
@@ -84,12 +77,12 @@ def deposito(): #Aumenta o saldo da conta com o valor do depósito.
         print("Transação não autorizada.")
         return
 
-    valor = float(input("Digite o valor a ser depositado: "))
-    while valor <= 0: #Evitando valores inválidos.
+    valor_deposito = float(input("Digite o valor a ser depositado: "))
+    while valor_deposito <= 0: #Evitando valores inválidos.
         print("Valor inválido. Por favor, tente novamente.")
-        valor = float(input("Digite o valor a ser depositado: "))
+        valor_deposito = float(input("Digite o valor a ser depositado: "))
 
-    saldos[indice] += valor #Finalização.
+    contas[indice] = contas[indice][:4] + (valor_deposito,) + contas[indice][5:]#Finalização.
     print("Depósito realizado com sucesso.")
     return 
 
@@ -108,11 +101,11 @@ def saque(): #Diminui o saldo da conta com o valor do saque.
         return
 
     valor_saque = float(input("Digite o valor a ser sacado: "))
-    while valor_saque < 0 or valor_saque > saldos[indice]: #Evitando valores inválidos.
+    while valor_saque < 0 or valor_saque > contas[indice][4]: #Evitando valores inválidos.
         print("Valor inválido. Por favor, tente novamente.")
         valor_saque = float(input("Digite o valor a ser sacado: "))
 
-    saldos[indice] -= valor_saque #Finalização.
+    contas[indice] = contas[indice][:4] + (contas[indice][4] - valor_saque,) + contas[indice][5:] #Finalização.
     print("Saque realizado com sucesso.")
     return
 
@@ -130,7 +123,7 @@ def consultar_saldo(): #Consulta o saldo da conta.
         print("Transação não autorizada.")
         return
     
-    print("Saldo atual: R$", saldos[indice]) #Finalização.
+    print("Saldo atual: R$", contas[indice][4]) #Finalização.
     return 
 
 
